468819a2799b15dc95f76e045b89bf75aa02e19d
Nicolás Palermo <50157861+niklauspalerme@users.noreply.github.com>
2021-07-02 10:38:52 -0300

    Update promesas.js

--- Meeting 19 - Promeses and Fetch/promesas.js
+++ Meeting 19 - Promeses and Fetch/promesas.js
@@ -28,3 +28,40 @@ miPromesa.then(result => {
 })
 
 
+//////////////////////////////////////////////////////////////////
+// Otro ejemplo
+
+let personas= [
+
+  {
+    nombre: "Maria",
+    edad: 25
+  },
+  {
+    nombre: "Claudia",
+    edad: 20
+  },{
+    nombre: "Veronica",
+    edad: 30
+  }
+
+]
+
+const buscarEdad = (edad) =>{
+
+  return new Promise((resolve,reject)=>{
+
+      let found = personas.filter(persona => persona.edad === edad)
+
+      if(found.length > 0)
+        resolve(found)
+      else
+        reject('No existe la persoma con esa edad')
+
+  })
+
+}
+
+buscarEdad(10)
+.then(response => console.log(response))
+.catch(error => console.log(error))
